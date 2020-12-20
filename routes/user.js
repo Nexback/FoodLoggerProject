50fe059f49306e2dfc41c35f20f626916b43a6af
Terence <tli46@buffalo.edu>
2020-12-20 14:27:11 -0500

    Added sign up functionality

--- routes/user.js
+++ routes/user.js
@@ -4,8 +4,41 @@ const bcrypt = require('bcryptjs');
 const e = require('express');
 const { reset } = require('nodemon');
 var path = require('path');
+const Users = require('../models/Users');
 
 module.exports = app => {
     mongoose.set('useFindAndModify', false);
+
+    app.post('/signup', async(req, res) => {
+        try{
+            const email = req.body.email;
+            const password = req.body.password;
+            const fName = req.body.firstName;
+            const lName = req.body.lastName;
+            const birthday = req.body.birthday;
+
+            Users.findOne({email: email}, async function(err, user){
+                if(!user){
+                    const newUser = new Users();
+                    newUser.firstName = fName;
+                    newUser.lastName = lName;
+                    newUser.email = email;
+                    newUser.birthday = birthday;
+                    newUser.password = bcrypt.hashSync(password, 12);
+
+                    const user = await newUser.save();
+
+                    res.send(200).send(user);
+                }
+                else{
+                    res.send("This user already exists.");
+                }
+            })
+        }
+        catch (err){
+            console.log("Pain incoming.");
+            res.sendStatus(400);
+        }
+    })
 
 }
